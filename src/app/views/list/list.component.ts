87664644f0163e6dd138d86cd92df607402eccac
soareseric <contato.eric.sm@gmail.com>
2019-01-18 15:37:36 -0200

    Event e Property Binding

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  selectedPkm = null;
+
   pokemonList = [
     { name: 'Bulbasaur', number: 1 },
     { name: 'Charmander', number: 4 },
@@ -14,9 +16,19 @@ export class ListComponent implements OnInit {
     { name: 'Pikachu', number: 25 }
   ]
 
+  get pkmSprite(){
+    const number = ('000' + this.selectedPkm.number).slice(-3);
+    return `//serebii.net/sunmoon/pokemon/${number}.png`;
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
 
+selectPokemon(pkm){
+  this.selectedPkm = pkm;
 }
+
+}
+
